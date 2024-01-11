c6db22bc3ffa3aed92a3f8df71875144e0f81c85
JFMajer <jakubmajer@live.com>
2024-01-11 19:56:58 +0100

    run function

--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+)
 
+func main() {
+	op := flag.String("op", "sum", "operation to execute")
+	column := flag.Int("col", 1, "column on which to execute operation")
+
+	flag.Parse()
+
+	if err := run(flag.Args(), *op, *column, os.Stdout); err != nil {
+		fmt.Println(os.Stderr, err)
+	}
+}
+
+func run(filenames []string, op string, column int, out io.Writer) error {
+	var opFunc statsFunc
+
+	if len(filenames) == 0 {
+		return ErrNoFiles
+	}
+
+	if column < 1 {
+		return fmt.Errorf("%w: %d", ErrInvalidColumn, column)
+	}
+
+	switch op {
+	case "sum":
+		opFunc = sum
+	case "avg":
+		opFunc = average
+	default:
+		return fmt.Errorf("%w: %s", ErrInvalidOperation, op)
+	}
 }
