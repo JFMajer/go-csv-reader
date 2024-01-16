7e570bd6a8fe50745a960bb221deef5032bd3b89
JFMajer <jakubmajer@live.com>
2024-01-16 19:44:25 +0100

    more performant csv2float function

--- csv.go
+++ csv.go
@@ -23,28 +23,40 @@ func average(data []float64) float64 {
 
 func csv2float(r io.Reader, column int) ([]float64, error) {
 	cr := csv.NewReader(r)
+	cr.ReuseRecord = true
 	column--
 
-	allData, err := cr.ReadAll()
-	if err != nil {
-		return nil, fmt.Errorf("cannot read data from file: %w", err)
-	}
+	// allData, err := cr.ReadAll()
+	// if err != nil {
+	//     return nil, fmt.Errorf("cannot read data from file: %w", err)
+	// }
 
 	var data []float64
 
-	for i, row := range allData {
+	for i := 0; ; i++ {
+		row, err := cr.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, fmt.Errorf("cannot read data from file: %w", err)
+		}
+
 		if i == 0 {
 			continue
 		}
-		if len(row) < column {
-			return nil, fmt.Errorf("%w: File has only %d columns", ErrInvalidColumn, len(row))
+
+		if len(row) <= column {
+			return nil, fmt.Errorf("invalid column index")
 		}
+
 		v, err := strconv.ParseFloat(row[column], 64)
 		if err != nil {
-			return nil, fmt.Errorf("%w: %s", ErrNotNumber, err)
+			return nil, fmt.Errorf("cannot parse data from file: %w", err)
 		}
+
 		data = append(data, v)
 	}
-	return data, nil
 
+	return data, nil
 }
